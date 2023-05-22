0216d7eee8c6a04d6b6c5c2121905d58705c1045
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-05-22 16:59:59 +0400

    fix: delete commented code

--- src/store/modules/VaccineCondition/mutations.js
+++ src/store/modules/VaccineCondition/mutations.js
@@ -3,8 +3,5 @@ export default {
     state.inputs.had_vaccine = payload.had_vaccine
     state.inputs.vaccination_stage = payload.vaccination_stage
     state.inputs.i_am_waiting = payload.i_am_waiting
-    // console.log(payload.condition)
-
-    // state.inputs = { ...payload }
   }
 }
